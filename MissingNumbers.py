d2570b0b95a85f803a4d8720d8f6b700aad2125c
Vadym Oliinyk <74012247+v-v-oliinyk@users.noreply.github.com>
2025-01-15 14:39:53 +0100

    second try.

--- MissingNumbers.py
+++ MissingNumbers.py
@@ -1,16 +1,25 @@
-numbers1 = list(map(int, input("Please enter the sequence of numbers divided by comma: ").split(', ')))
+# numbers1 = list(map(int, input("Please enter the sequence of numbers divided by comma: ").split(', ')))
+numbers1 = list(input("Please enter the sequence of numbers divided by comma: ").split(', '))
 numbers2 = []
 
 # I wanted to create an "IF" for the input. but it makes an error automatically :D it's already enough.
+if not numbers1 or len(numbers1) == 1:
+    print("Please try again.")
+    exit()
 
 def find_min_max(nums):
-    return int(min(nums)), int(max(nums)) + 1
+    return int(min(nums)), int(max(nums))
 
 min_num, max_num = find_min_max(numbers1)
 
-for number in range(min_num, max_num):
-     numbers2.append(number)
+for number in range(min_num, max_num + 1):
+     numbers2.append(str(number))
 
-numbers_diff = list(set(numbers2).difference(set(numbers1))) #found easily in Internet how to compare lists.
+numbers_diff = set(numbers2).difference(set(numbers1)) #found easily in Internet how to compare lists.
 
-print(f"For '{numbers1}', the missing numbers are: '{numbers_diff}'")
+for number in numbers_diff:
+    numbers_diff.remove(number)
+    numbers_diff.add(int(number))
+
+
+print(f"For '{numbers1}', the missing numbers are: '{sorted(numbers_diff)}'")
